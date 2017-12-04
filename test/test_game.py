77be2196c73e86cf88beb46a393eb71f12df556d
alkamid <adamkli@gmail.com>
2017-12-04 20:10:44 +0000

    Reading testgame in fixture + more tests

--- test/test_game.py
+++ test/test_game.py
@@ -1,13 +1,18 @@
+import pytest
 from ..bag import Bag
 from ..board import Board
+from ..game import Game
 
-
-def test_all_moves():
+@pytest.fixture
+def test_game_text():
     with open('test/test_game.txt') as f:
         text = f.read()
+        return text
+
+def test_all_moves(test_game_text):
 
     B = Board()
-    B.read_from_text(text)
+    B.read_from_text(test_game_text)
     words = set(B.find_all_words())
 
     assert words == set(['EL', 'MAJ', 'MI', 'LA', 'PUCZ', 'ON', 'EN', 'WJAZDY', 'TY', 'WAS', 'REW', 'SĄD', 'PŁACE',
@@ -16,12 +21,10 @@ def test_all_moves():
                          'SEM', 'KOŃ', 'GAZDĄ', 'PI', 'HYR', 'AL', 'EŚ', 'JAKÓW'])
 
 
-def test_letters_left():
-    with open('test/test_game.txt') as f:
-        text = f.read()
+def test_letters_left(test_game_text):
 
     B = Board()
-    B.read_from_text(text)
+    B.read_from_text(test_game_text)
     print(B)
     B.find_all_words()
 
@@ -31,4 +34,14 @@ def test_letters_left():
             if col != '':
                 bag.remove(col)
 
-    print(bag)
+    assert bag.remaining() == {'M': -1, 'Y': -1, '_': 2}
+
+
+def test_players_name(test_game_text):
+    g = Game(test_game_text)
+    assert g.player1.nickname == 'alkamid'
+    assert g.player2.nickname == 'amarant53'
+
+def test_first_move():
+    pass
+
